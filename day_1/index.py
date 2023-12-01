db4a6f201c290c35047c5863dbe7c1b99377ea3a
david_funnell <dfunnell77@gmail.com>
2023-12-01 10:23:59 -0800

    added replace method git push

--- day_1/index.py
+++ day_1/index.py
@@ -3,77 +3,96 @@
 
 import time
 
-# def day_1():
-#     total = 0
+def day_1():
+    total = 0
 
-#     for line in open("day_1/data.txt", "r"):
-#         l = 0
-#         r = len(line)-1
-#         while l <= r:
-#             if line[l].isnumeric() == False:
-#                 l += 1
-#             if line[r].isnumeric() == False:
-#                 r -= 1
-#             if line[l].isnumeric() and line[r].isnumeric():
-#                 total += int(line[l] + line[r])
-#                 break
-#     return total
+    for line in open("day_1/data.txt", "r"):
 
-# print(day_1())
+        line = (
+            line.replace("one", "one1one")
+            .replace("two", "two2two")
+            .replace("three", "three3three")
+            .replace("four", "four4four")
+            .replace("five", "five5five")
+            .replace("six", "six6six")
+            .replace("seven", "seven7seven")
+            .replace("eight", "eight8eight")
+            .replace("nine", "nine9nine")
+)
+        l = 0
+        r = len(line)-1
+        while l <= r:
+            if line[l].isnumeric() == False:
+                l += 1
+            if line[r].isnumeric() == False:
+                r -= 1
+            if line[l].isnumeric() and line[r].isnumeric():
+                total += int(line[l] + line[r])
+                break
+    end = time.time()
+
+    print(end-start)
+
+    return total
+
+start = time.time()
+
+print(day_1())
 
 
-# def day_1():
-#     total = 0
 
-#     numdict = {
-#         "one" : "1",
-#         "two" : "2",
-#         "three": "3",
-#         "four" : "4",
-#         "five" : "5",
-#         "six" : "6",
-#         "seven" : "7",
-#         "eight" : "8",
-#         "nine" : "9"
-#     }
+def day_1():
+    total = 0
 
-#     for line in open("day_1/data.txt", "r"):
-#         first = None
+    numdict = {
+        "one" : "1",
+        "two" : "2",
+        "three": "3",
+        "four" : "4",
+        "five" : "5",
+        "six" : "6",
+        "seven" : "7",
+        "eight" : "8",
+        "nine" : "9"
+    }
+
+    for line in open("day_1/data.txt", "r"):
+        first = None
         
-#         for index in range(len(line)):
-#             if line[index].isnumeric():
-#                 first = line[index]
-#                 break
-#             else:
-#                 for valkey in numdict:
-#                     if line[index:len(valkey)+index] == valkey:
-#                         first = numdict[valkey]
-#                         break
-#                 if first != None:
-#                     break
-#         second = None
-#         for index in range(len(line)-1, -1, -1):
-#             if line[index].isnumeric():
-#                 second = line[index]
-#                 break
-#             else:
-#                 for valkey in numdict:
-#                     if line[index:len(valkey)+index] == valkey:
-#                         second = numdict[valkey]
-#                         break
-#                 if second != None:
-#                     break
-#         total += int(first+ second)
+        for index in range(len(line)):
+            if line[index].isnumeric():
+                first = line[index]
+                break
+            else:
+                for valkey in numdict:
+                    if line[index:len(valkey)+index] == valkey:
+                        first = numdict[valkey]
+                        break
+                if first != None:
+                    break
+        second = None
+        for index in range(len(line)-1, -1, -1):
+            if line[index].isnumeric():
+                second = line[index]
+                break
+            else:
+                for valkey in numdict:
+                    if line[index:len(valkey)+index] == valkey:
+                        second = numdict[valkey]
+                        break
+                if second != None:
+                    break
+        total += int(first+ second)
 
-#     end = time.time()
+    end = time.time()
 
-#     print(end-start)
+    print(end-start)
 
-#     return total
+    return total
 
-# start = time.time()
+start = time.time()
 
-# print(day_1())
+print(day_1())
 
 
 def day_1():
