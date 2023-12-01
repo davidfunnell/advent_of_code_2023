d4e2fff01c7ed8f6d0a00f25368dbec5d9da8116
david_funnell <dfunnell77@gmail.com>
2023-12-01 00:28:31 -0800

    alternative solution to part 2.

--- day_1/index.py
+++ day_1/index.py
@@ -1,5 +1,8 @@
 # https://adventofcode.com/2023/day/1
 
+
+import time
+
 # def day_1():
 #     total = 0
 
@@ -18,7 +21,60 @@
 
 # print(day_1())
 
-# abcone2threexyz
+
+# def day_1():
+#     total = 0
+
+#     numdict = {
+#         "one" : "1",
+#         "two" : "2",
+#         "three": "3",
+#         "four" : "4",
+#         "five" : "5",
+#         "six" : "6",
+#         "seven" : "7",
+#         "eight" : "8",
+#         "nine" : "9"
+#     }
+
+#     for line in open("day_1/data.txt", "r"):
+#         first = None
+        
+#         for index in range(len(line)):
+#             if line[index].isnumeric():
+#                 first = line[index]
+#                 break
+#             else:
+#                 for valkey in numdict:
+#                     if line[index:len(valkey)+index] == valkey:
+#                         first = numdict[valkey]
+#                         break
+#                 if first != None:
+#                     break
+#         second = None
+#         for index in range(len(line)-1, -1, -1):
+#             if line[index].isnumeric():
+#                 second = line[index]
+#                 break
+#             else:
+#                 for valkey in numdict:
+#                     if line[index:len(valkey)+index] == valkey:
+#                         second = numdict[valkey]
+#                         break
+#                 if second != None:
+#                     break
+#         total += int(first+ second)
+
+#     end = time.time()
+
+#     print(end-start)
+
+#     return total
+
+# start = time.time()
+
+# print(day_1())
+
 
 def day_1():
     total = 0
@@ -36,33 +92,42 @@ def day_1():
     }
 
     for line in open("day_1/data.txt", "r"):
-        first = None
-        
-        for index in range(len(line)):
-            if line[index].isnumeric():
-                first = line[index]
-                break
-            else:
-                for valkey in numdict:
-                    if line[index:len(valkey)+index] == valkey:
-                        first = numdict[valkey]
-                        break
-                if first != None:
-                    break
+
+        first = None    
         second = None
-        for index in range(len(line)-1, -1, -1):
-            if line[index].isnumeric():
-                second = line[index]
+        l = 0
+        r = len(line)-1
+        
+        while l <= r:
+            if first is None:
+                if line[l].isnumeric():
+                    first = line[l]
+                else:
+                    for valkey in numdict:
+                        if line[l:len(valkey)+l] == valkey:
+                            first = numdict[valkey]
+                    if first is None:
+                        l+=1
+            if second is None:
+                if line[r].isnumeric():
+                    second = line[r]
+                else:
+                    for valkey in numdict:
+                        if line[r:len(valkey)+r] == valkey:
+                            second = numdict[valkey]
+                    if second is None:
+                        r -= 1
+            if first is not None and second is not None:
                 break
-            else:
-                for valkey in numdict:
-                    if line[index:len(valkey)+index] == valkey:
-                        second = numdict[valkey]
-                        break
-                if second != None:
-                    break
+                        
         total += int(first+ second)
+    
+    end = time.time()
+
+    print(end-start)
 
     return total
+
+start = time.time()
 
 print(day_1())
